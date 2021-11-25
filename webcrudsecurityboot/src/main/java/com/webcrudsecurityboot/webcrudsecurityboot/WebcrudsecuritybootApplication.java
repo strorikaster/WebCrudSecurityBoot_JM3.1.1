e7bd4abeb7b31a4bddc1784f1381dbfc64bf6557
strorikaster <strorikaster@gmail.com>
2021-11-26 00:36:56 +0300

    Add files via upload

---
+++ webcrudsecurityboot/src/main/java/com/webcrudsecurityboot/webcrudsecurityboot/WebcrudsecuritybootApplication.java
@@ -0,0 +1,13 @@
+package com.webcrudsecurityboot.webcrudsecurityboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcrudsecuritybootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcrudsecuritybootApplication.class, args);
+	}
+
+}
